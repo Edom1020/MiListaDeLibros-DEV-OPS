e51a04a206d398c7e7e8a5a6a340a3e9236d2d95
kevin ochoa <kochoao@unicartagena.edu.co>
2026-05-25 22:10:41 -0500

    CORS configurado correctamente

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'https://mi-lista-de-libros-dev-ds9p31hex-esteban-dominguez-s-projects.vercel.app',
     'https://mi-lista-de-libros-dev-ops.vercel.app'
   ],
   credentials: true
